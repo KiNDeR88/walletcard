75d94348936e8d1ee7225ae5b646c46d50730513
Алексей Меляков <Polzovatel@MacBook-Pro-Ben.local>
2025-10-16 12:29:31 +0300

    initial commit: loyalty registration with admin panel

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT: set base for GitHub Pages (repo name)
+  base: process.env.NODE_ENV === 'production' ? '/walletcard/' : '/',
 })
